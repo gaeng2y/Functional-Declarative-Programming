7ffd235e537cf4a39b87f7208b68d413a1032075
Kyeongmo(Kyle) Yang <gaeng2y@gmail.com>
2025-04-03 19:36:02 +0900

    Update Contents.swift

--- week1/GuGuClass.playground/Contents.swift
+++ week1/GuGuClass.playground/Contents.swift
@@ -25,3 +25,23 @@ func printTimesTable(num: Int, until max: Int) -> String {
 
 // 사용 예시
 printTimesTable(num: 2, until: 9)
+
+func multiply(x: Int, y: Int) -> Int {
+    x * y
+}
+
+func printFormula(
+    with Formula: String,
+    closure: (Int, Int) -> Int)
+) -> (Int, Int) -> Int {
+    return { x, y in
+            print("\(formula) = \(closure(x,y))")
+    }
+}
+
+func printMultiply(with num: Int, from start: Int, until end: Int) {
+    guard start <= end else {
+        return
+    }
+    printMultiply(with: num, from: start, until: end)
+}
